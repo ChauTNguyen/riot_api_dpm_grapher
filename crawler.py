0ee801ccad20fa54de24b8abda5434689b73e474
ChauTNguyen <ChauTNguyen96@gmail.com>
2016-04-13 20:34:17 -0700

    automated the participant_ids. added some other functions to do this too.

---
+++ crawler.py
@@ -0,0 +1,23 @@
+import config
+import requests
+
+KEY = config.key
+
+REGION_ENDPOINT = "https://na.api.pvp.net/api/lol/"
+
+def get_match(region, matchId):
+    """
+    Retrieve match by match ID.
+    """
+    return requests.get(
+        REGION_ENDPOINT + region + "/v2.2/match/" + str(matchId) + "?api_key=" + str(KEY)
+        ).json()
+
+def get_champion_name(championId):
+    return requests.get(
+        "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/" + str(championId) + "?api_key=" + KEY
+    ).json()['key']
+
+def get_total_damage_dealt_by_id(response, participantId):
+    return response['participants'][participantId-1]['stats']['totalDamageDealtToChampions']
+    # participantId is +1 in participantIdentities... must account for that
--- crawler_4_jynthe.py
+++ crawler_4_jynthe.py
@@ -1,5 +1,4 @@
-from crawler import get_match, get_champion_name
-import config
+from crawler import get_match, get_champion_name, get_total_damage_dealt_by_id, config
 
 KEY = config.key
 region = "na"
@@ -9,10 +8,18 @@ region = "na"
 match_ids = [2151814134, 2151746897, 2151117303, 2151113256, 2151068241, 2151070857,
              2148985988, 2148939120, 2148931983, 2148854194]
 participants_ids = [1, 3, 5, 8, 1, 1, 8, 8, 1, 7]
-champions_ids = [22, 81, 202, 96, 236, 236, 42, 42, 15, 202]
+champions_ids = [22, 81, 202, 22, 236, 236, 42, 42, 15, 202]
+summoner_id = 49188821
+
 # count : 10
 
 for i in range(0, len(match_ids)):
     current_match_response = get_match(region, str(match_ids[i]))
+
+    for n in range(0, 10):
+        if current_match_response['participantIdentities'][n]['player']['summonerId'] == summoner_id:
+            _participant_id = current_match_response['participantIdentities'][n]['participantId']
+
     print("Game: " + str(i + 1) + " - " + get_champion_name(champions_ids[i]) + " - Total damage dealt",
-          current_match_response['participants'][participants_ids[i]]['stats']['totalDamageDealtToChampions'])
+                get_total_damage_dealt_by_id(current_match_response, _participant_id)
+          ) # participant_id must be - 1
