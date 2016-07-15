7042b38629de57e7950628e3e1d809638c0fda4f
Chau Nguyen <chautnguyen96@gmail.com>
2016-07-14 22:58:45 -0700

    fixed the oh so important rate limitergit add src/crawler.py src/functions.py got the idea from working on and fixing my rate limiter in a different project. not gonna clean up the urls on this project though haha, no need to

--- src/crawler.py
+++ src/crawler.py
@@ -15,6 +15,7 @@ def crawl_dpm(summoner_id):
             int(get_match_duration(curr_match_res) / 60)
             )
         )
+        # print(get_champion_name(get_champ_id(curr_match_res, get_participant_id(curr_match_res, summoner_id))))
 
     return dpms
 
@@ -32,5 +33,6 @@ def crawl_avg_dpm(summoner_id):
         total_dmg += int(get_total_damage_dealt_by_id(
             curr_match_res, get_participant_id(curr_match_res, summoner_id))
         )
+        sleep(1.5)
 
     return calculate_dpm(total_dmg, total_game_secs / 60)
--- src/functions.py
+++ src/functions.py
@@ -3,6 +3,7 @@ from time import sleep
 
 import src.config as config
 from .config import NUM_OF_GAMES
+
 KEY = config.key
 
 REGION_ENDPOINT = "https://" + config.REGION + ".api.pvp.net/api/lol/"
@@ -11,9 +12,8 @@ TOO_MANY_REQUESTS = 429
 
 # These functions grab responses.
 def get_summoner_id(summoner_name):
-    return requests.get(
-        REGION_ENDPOINT + config.REGION + "/v1.4/summoner/by-name/" + summoner_name + "?api_key=" + KEY
-    ).json()[summoner_name]['id']
+    url = REGION_ENDPOINT + config.REGION + "/v1.4/summoner/by-name/" + summoner_name + "?api_key=" + KEY
+    return rate_limiter(requests.get(url), url).json()[summoner_name]['id']
 
 
 def get_match_list(summoner_id):
@@ -24,28 +24,31 @@ def get_match_list(summoner_id):
 
 
 def get_match(matchId):
-    match_response = rate_limiter(requests.get(REGION_ENDPOINT + config.REGION + "/v2.2/match/" + str(matchId) + "?api_key=" + str(KEY)))
+    url = REGION_ENDPOINT + config.REGION + "/v2.2/match/" + str(matchId) + "?api_key=" + str(KEY)
+    match_response = rate_limiter(
+        requests.get(url), url)
     print(" => SUCCESS")
     return match_response.json()
 
 
 def get_champion_name(champion_id):
     """Return a champion name corresponding to the champion id."""
-    return requests.get(
-        "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/" + str(champion_id) + "?api_key=" + KEY
+    url = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/" + str(champion_id) + "?api_key=" + KEY
+    return rate_limiter(
+        requests.get(url), url
     ).json()['key']
 
 
 # These functions below use a match response to return specific information.
 def get_total_damage_dealt_by_id(match_response, participant_id):
     """Return the total damage dealt by participant id within a match."""
-    return match_response['participants'][participant_id-1]['stats']['totalDamageDealtToChampions']
+    return match_response['participants'][participant_id - 1]['stats']['totalDamageDealtToChampions']
     # participantId is +1 in participantIdentities (within the standard riot json file) ... must account for that
 
 
 def get_champ_id(match_response, participantId):
     """Return the champion id by participant id within a match."""
-    return match_response['participants'][participantId-1]['championId']
+    return match_response['participants'][participantId - 1]['championId']
 
 
 def get_match_duration(match_response):
@@ -57,10 +60,8 @@ def get_participant_id(match_response, summoner_id):
     """Return the participantId of the participant within a match."""
     for n in range(0, 10):
         # Find the participant ID of the summoner in the current match.
-        if rate_limiter(match_response['participantIdentities'][n]['player']['summonerId']) == summoner_id:
-            sleep(1.5)
-            return rate_limiter(match_response['participantIdentities'][n]['participantId'])
-        sleep(.5)
+        if match_response['participantIdentities'][n]['player']['summonerId'] == summoner_id:
+            return match_response['participantIdentities'][n]['participantId']
 
 
 def get_matches_with_role(list_response, role, num_of_games):
@@ -85,11 +86,11 @@ def get_matches_with_role(list_response, role, num_of_games):
     count = 0
     i = 0
     while count != num_of_games:
-        role_in_match = rate_limiter(list_response['matches'][i]['role'])
+        role_in_match = list_response['matches'][i]['role']
 
         if role_in_match == role:
-            sleep(1.5)
-            match_with_role = rate_limiter(list_response['matches'][i]['matchId'])
+            sleep(.75)
+            match_with_role = list_response['matches'][i]['matchId']
             match_ids.append(match_with_role)
             count += 1
             print("LOADED", count, "/", NUM_OF_GAMES)
@@ -99,17 +100,19 @@ def get_matches_with_role(list_response, role, num_of_games):
 
 
 # Extra functions
-def rate_limiter(response):
-    while str(response) == str(TOO_MANY_REQUESTS):
+def rate_limiter(response, url):
+    while str(response.status_code) == str(TOO_MANY_REQUESTS):
+        print(response.status_code)
         print("\n\tExceeded rate limit.")
         print("\tTrying again in 5 seconds...")
         sleep(5)
+        response = requests.get(url)
     return response
 
 
 def convert_to_minutes_seconds(seconds):
     a = seconds / 60
-    a,b = divmod(a, 1.0)
+    a, b = divmod(a, 1.0)
     b *= 60
     c = str(round(b))
 
